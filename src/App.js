cfdc0f77e0e0916a7a50dcd3941e35026895b3ad
Selvaraj-p <110965961+Selvaraj-p@users.noreply.github.com>
2023-12-29 17:30:59 +0530

    changes updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
-
+import styles from './App.module.css'
 
 function App() {
 
   
   return (
     <>
-      <Navbar />
+      <Navbar className={styles.sectionWrapper}/>
+      
 
     </>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./Navbar.module.css"
 
 export default function Navbar(){
     return(
         <>
             <nav>
+                <button className={styles.nav}><b>hello World</b></button>
 
 
             </nav>
